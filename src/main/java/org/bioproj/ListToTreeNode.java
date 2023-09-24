6868de9c6e6f35a4709aeae4b0f33f5d04d4ac88
wangyang <1749748955@qq.com>
2023-09-24 12:51:28 +0800

    add tree node

---
+++ src/main/java/org/bioproj/ListToTreeNode.java
@@ -0,0 +1,60 @@
+package org.bioproj;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class ListToTreeNode {
+    public static TreeNode convertToTree(List<Integer> list) {
+        return convertToTree(list, 0, list.size() - 1);
+    }
+
+    private static TreeNode convertToTree(List<Integer> list, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+
+        int mid = (start + end) / 2;
+        TreeNode root = new TreeNode(list.get(mid));
+
+        root.left = convertToTree(list, start, mid - 1);
+        root.right = convertToTree(list, mid + 1, end);
+
+        return root;
+    }
+    public static TreeNode fillTree(List<Integer> list) {
+        if (list == null || list.isEmpty()) {
+            return null;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        TreeNode root = new TreeNode(list.get(0));
+        queue.offer(root);
+
+        int i = 1; // list 索引
+
+        while (i < list.size()) {
+            TreeNode currNode = queue.poll();
+
+            // 填充左子节点
+            if (i < list.size()) {
+                Integer leftVal = list.get(i++);
+                if (leftVal != null) {
+                    currNode.left = new TreeNode(leftVal);
+                    queue.offer(currNode.left);
+                }
+            }
+
+            // 填充右子节点
+            if (i < list.size()) {
+                Integer rightVal = list.get(i++);
+                if (rightVal != null) {
+                    currNode.right = new TreeNode(rightVal);
+                    queue.offer(currNode.right);
+                }
+            }
+        }
+
+        return root;
+    }
+}
---
+++ src/main/java/org/bioproj/TreeNode.java
@@ -0,0 +1,13 @@
+package org.bioproj;
+
+import lombok.Data;
+
+@Data
+public class TreeNode {
+    private int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
