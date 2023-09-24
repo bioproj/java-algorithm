102b4b1b1de57eb6525b491ef55a22b8a8c58f68
wangyang <1749748955@qq.com>
2023-09-24 13:25:04 +0800

    add docs

--- src/main/java/org/bioproj/Main.java
+++ src/main/java/org/bioproj/Main.java
@@ -5,12 +5,36 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7));
-//        TreeNode root = ListToTreeNode.convertToTree(list);
-        TreeNode root = ListToTreeNode.fillTree(list);
+    public static TreeNode lcaDeepestLeaves(TreeNode root) {
+        if(root == null){
+            return null;
+        }
 
-        System.out.println(root);
+        int left = height(root.left);
+        int right = height(root.right);
+        if(left == right){
+            return root;
+        }else if(left > right){
+            return lcaDeepestLeaves(root.left);
+        }else{
+            return lcaDeepestLeaves(root.right);
+        }
+    }
+
+    private static int height(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(Arrays.asList(3,5,1,6,2,0,8,null,null,7,4));
+//        TreeNode root = ListToTreeNode.convertToTree(list);
+        TreeNode treeNode = ListToTreeNode.fillTree(list);
+        TreeNode result = lcaDeepestLeaves(treeNode);
+
+        System.out.println(treeNode);
 
     }
 
